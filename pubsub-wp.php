7de70b61b2a3ff929ab63d166216d68d6d85f09d
iftakharul-islam <iftakharulislam881@gmail.com>
2024-06-01 00:23:44 +0600

    New plugin init

---
+++ pubsub-wp.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Pubsub Wp
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     pubsub-wp
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Pubsub_Wp
+ */
+
+// Your code starts here.
